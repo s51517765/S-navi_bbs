c9925a15602ec9487eee69eee17f90247c352539
N SHINYA <nakasha348@gmail.com>
2026-04-25 18:49:23 +0900

    インデックス用サイトタイトルを設定

--- board/context_processors.py
+++ board/context_processors.py
@@ -7,6 +7,8 @@ import os
 def const_settings(request):
     return {
         "SITE_TITLE": os.environ.get("SITE_TITLE", "デフォルトのサイト名"),
+        "INDEX_TITLE": os.environ.get("INDEX_TITLE", "デフォルトのサイト名"),
+        "DESCRIPTION": os.environ.get("DESCRIPTION", "デフォルトのサイトの説明"),
         "POST_REWARD": os.environ.get("POST_REWARD", 30),
         "GOOD_REWARD": os.environ.get("GOOD_REWARD", 1),
         "BAD_REWARD": os.environ.get("BAD_REWARD", 1),
